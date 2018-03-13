abd2f7ba8b12a2f058d497883aa63067f52eff57
yixingw <wangyixingvictor@gmail.com>
2018-03-13 13:20:26 -0500

    changes in P5

--- P5.js
+++ P5.js
@@ -27,8 +27,8 @@ function landing_login() {
 	debugger;
 	if (document.getElementById('uname').value == "sfreedman14" && document.getElementById("psw").value == "password") {
 		var username = "sfreedman14";
-		localStorage.setItem("username", username);
-		window.location.replace('home.html');
+		window.location.replace('home.html');		localStorage.setItem("username", username);
+
 	} else if (document.getElementById('uname').value == "cokoroafor" && document.getElementById("psw").value == "shnoyes") {
 		var username = "cokoroafor";
 		localStorage.setItem("username", username);
